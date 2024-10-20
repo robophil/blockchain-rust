25a1446fe3f9a6f8641c45b0b528c08e57a49c74
roophil <6032018+robophil@users.noreply.github.com>
2024-10-20 04:35:57 +0200

    feat: added sample data to mine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod engine;
+
+use crate::engine::{block, blockchain};
+
 fn main() {
-    println!("Hello, world!");
+    let mut bc = blockchain::Blockchain::new();
+    bc.mine_block(5, vec![
+        block::Data::Blob(block::Blob {
+            data: String::from("Wie geht's Welt???"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Transaction(block::Transaction {
+            sender: String::from("Eva"),
+            receiver: String::from("Victor"),
+            amount: 100.0,
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+    ]);
+
+    bc.mine_block(5, vec![
+        block::Data::Blob(block::Blob {
+            data: String::from("Hello, World!"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Transaction(block::Transaction {
+            sender: String::from("Eva"),
+            receiver: String::from("Victor"),
+            amount: 100.0,
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+    ]);
+
+    bc.log();
 }
