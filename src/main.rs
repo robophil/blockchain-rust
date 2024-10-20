232fa094d8f77f5901edae67cde9de83356c311c
roophil <6032018+robophil@users.noreply.github.com>
2024-10-20 21:33:17 +0200

    fix: add more blocks to be mined

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use crate::engine::{block, blockchain};
 
 fn main() {
     let mut bc = blockchain::Blockchain::new();
-    bc.mine_block(5, vec![
+    bc.mine_block(6, vec![
         block::Data::Blob(block::Blob {
             data: String::from("Wie geht's Welt???"),
             timestamp: blockchain::Blockchain::get_current_timestamp(),
@@ -17,18 +17,43 @@ fn main() {
         }),
     ]);
 
-    bc.mine_block(5, vec![
+    bc.mine_block(6, vec![
         block::Data::Blob(block::Blob {
             data: String::from("Hello, World!"),
             timestamp: blockchain::Blockchain::get_current_timestamp(),
         }),
         block::Data::Transaction(block::Transaction {
             sender: String::from("Eva"),
-            receiver: String::from("Victor"),
-            amount: 100.0,
+            receiver: String::from("Robo"),
+            amount: 200.0,
             timestamp: blockchain::Blockchain::get_current_timestamp(),
         }),
     ]);
 
-    bc.log();
+    bc.mine_block(6, vec![
+        block::Data::Blob(block::Blob {
+            data: String::from("Goodbye, World!"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Transaction(block::Transaction {
+            sender: String::from("Oluka"),
+            receiver: String::from("Robo"),
+            amount: 200.0,
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Blob(block::Blob {
+            data: String::from("Goodbye, World!"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Blob(block::Blob {
+            data: String::from("Hola"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+        block::Data::Blob(block::Blob {
+            data: String::from("Ciao"),
+            timestamp: blockchain::Blockchain::get_current_timestamp(),
+        }),
+    ]);
+
+    bc.log_blockchain();
 }
